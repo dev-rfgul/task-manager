13a8b7360b1c50ce22f886396743b481b57c9e88
dev-rfgul <rfgul587@gmail.com>
2025-03-18 21:15:53 +0500

    finalized the ui of the login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+
 
 const App = () => {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}`);
-        const result = await response.json();
-        setData(result);
-        console.log(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
-    <div className='bg-green-600 p-4 text-white'>
-      <h1 className='text-xl font-bold mb-2'>Fetched Data:</h1>
-      <pre>{data ? JSON.stringify(data.message, null, 2) : 'Loading...'}</pre>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
