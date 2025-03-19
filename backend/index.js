51957a9154baed19cf126101a546ea424bf6653d
dev-rfgul <rfgul587@gmail.com>
2025-03-19 23:04:29 +0500

    now the cookies are alos working fine

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 
 
 
@@ -11,6 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json())
 connectDB();
+app.use(cookieParser());    
 
 const corsOptions = {
     Credential:true,
