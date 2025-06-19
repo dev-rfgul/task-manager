017f939e92ec2ade14b0fc5837cb5ef3bb4e3aeb
dev-rfgul <rfgul587@gmail.com>
2025-06-19 20:32:36 +0500

    fix: update allowedOrigins to include HTTPS for production

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 connectDB();
 
 app.use(cookieParser());
-const allowedOrigins = ['task-ai-tau.vercel.app', 'http://localhost:5173']
+const allowedOrigins = ['https://task-ai-tau.vercel.app', 'http://localhost:5173']
 const corsOptions = {
     origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
